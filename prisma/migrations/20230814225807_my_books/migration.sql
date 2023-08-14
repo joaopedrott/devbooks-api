565f9e8a8d18d9de548a77e4968407905a731f60
Matheus Castro <goca1377@gmail.com>
2023-08-14 19:59:00 -0300

    feat: add my-books migration

---
+++ prisma/migrations/20230814225807_my_books/migration.sql
@@ -0,0 +1,23 @@
+-- CreateEnum
+CREATE TYPE "BookState" AS ENUM ('IS_READING', 'READ', 'WANTS_TO_READ');
+
+-- CreateTable
+CREATE TABLE "MyBooks" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "bookId" TEXT NOT NULL,
+    "bookState" "BookState" NOT NULL DEFAULT 'WANTS_TO_READ',
+    "currentPage" INTEGER,
+    "totalPages" INTEGER NOT NULL,
+    "book" JSONB NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "MyBooks_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "MyBooks_bookId_key" ON "MyBooks"("bookId");
+
+-- AddForeignKey
+ALTER TABLE "MyBooks" ADD CONSTRAINT "MyBooks_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
